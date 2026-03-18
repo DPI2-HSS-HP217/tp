7c764b21b500de086550929c55d4306847aba892
Enzo <enzo.chung@u.nus.edu>
2026-03-18 13:15:34 +0800

    Fixed checkstyle issues

--- src/main/java/seedu/address/logic/commands/EditEnterCommand.java
+++ src/main/java/seedu/address/logic/commands/EditEnterCommand.java
@@ -1,10 +1,9 @@
 package seedu.address.logic.commands;
 
 import seedu.address.logic.ParserMode;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 
+/** Command to enter editing mode. */
 public class EditEnterCommand extends Command {
 
     public static final String COMMAND_WORD = "editmode";
--- src/main/java/seedu/address/logic/commands/EditExitCommand.java
+++ src/main/java/seedu/address/logic/commands/EditExitCommand.java
@@ -1,11 +1,10 @@
 package seedu.address.logic.commands;
 
 import seedu.address.logic.ParserMode;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 
-public class EditExitCommand extends Command{
+/** Command to exit editing mode. */
+public class EditExitCommand extends Command {
 
     public static final String COMMAND_WORD = "exitedit";
     public static final String MESSAGE_EXIT_EDITING_MODE_ACKNOWLEDGEMENT = "Exiting Editing mode as requested ...";
